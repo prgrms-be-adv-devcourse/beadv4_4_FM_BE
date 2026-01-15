59c395fba50349a463b58b76cf672f7ef56e1208
ddubuk228 <131105977+ddubuk228@users.noreply.github.com>
2026-01-15 17:17:10 +0900

    [Feature] #12 상품 등록 기능 추가 (#26)
    * Feat : 상품 등록 기능 추가

--- src/main/java/backend/mossy/boundedContext/cash/domain/wallet/CashUser.java
+++ src/main/java/backend/mossy/boundedContext/cash/domain/wallet/CashUser.java
@@ -1,7 +1,12 @@
 package backend.mossy.boundedContext.cash.domain.wallet;
 
 import backend.mossy.shared.member.domain.ReplicaMember;
+<<<<<<< HEAD:src/main/java/backend/mossy/boundedContext/cash/domain/wallet/CashUser.java
 import backend.mossy.shared.member.domain.ReplicaUser;
+=======
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+>>>>>>> 9591ad7 ([Feature] #12 상품 등록 기능 추가 (#26)):src/main/java/backend/mossy/boundedContext/cash/domain/wallet/CashMember.java
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
