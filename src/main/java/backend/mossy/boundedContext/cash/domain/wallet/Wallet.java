9591ad75c6e8eb6196ead0a0f72269b3c9ddf838
ddubuk228 <131105977+ddubuk228@users.noreply.github.com>
2026-01-15 17:17:10 +0900

    [Feature] #12 상품 등록 기능 추가 (#26)
    * Feat : 상품 등록 기능 추가

--- src/main/java/backend/mossy/boundedContext/cash/domain/wallet/Wallet.java
+++ src/main/java/backend/mossy/boundedContext/cash/domain/wallet/Wallet.java
@@ -4,12 +4,9 @@ import backend.mossy.boundedContext.cash.domain.history.CashLog;
 import backend.mossy.global.jpa.entity.BaseManualIdAndTime;
 import static jakarta.persistence.CascadeType.PERSIST;
 import static jakarta.persistence.CascadeType.REMOVE;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
@@ -20,10 +17,11 @@ import lombok.NoArgsConstructor;
 @Table(name = "CASH_WALLET")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@AttributeOverride(name = "id", column = @Column(name = "wallet_id"))
 public class Wallet extends BaseManualIdAndTime {
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private CashMember member;
 
     private long balance = 0;
--- src/main/java/backend/mossy/boundedContext/market/in/ApiV1ProductController.java
+++ src/main/java/backend/mossy/boundedContext/market/in/ApiV1ProductController.java
@@ -1,14 +1,17 @@
 package backend.mossy.boundedContext.market.in;
 
 import backend.mossy.boundedContext.market.app.MarketFacade;
+import backend.mossy.boundedContext.market.domain.Product;
+import backend.mossy.global.rsData.RsData;
+import backend.mossy.shared.market.dto.requets.ProductRequest;
 import backend.mossy.shared.market.dto.response.ProductResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +29,12 @@ public class ApiV1ProductController {
                 .stream()
                 .map(ProductResponse::from)
                 .toList();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RsData<Void> createProduct(@RequestBody @Valid ProductRequest request) {
+        marketFacade.registerProduct(request);
+        return new RsData<>("201", "상품이 등록되었습니다");
     }
 }
