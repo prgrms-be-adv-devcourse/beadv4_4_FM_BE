374c8d3ec862e0b51b909d2f0f97c05c8f3ebea9
nam3280 <nam3280@naver.com>
2026-01-15 14:21:20 +0900

    Fix : 주문/장바구니 도메인 수정

--- src/main/java/backend/mossy/boundedContext/market/domain/Order.java
+++ src/main/java/backend/mossy/boundedContext/market/domain/Order.java
@@ -14,9 +14,10 @@ import static jakarta.persistence.FetchType.LAZY;
 @Table(name = "MARKET_ORDER")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@AttributeOverride(name = "id", column = @Column(name = "order_id"))
 public class Order extends BaseIdAndTime {
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private MarketUser buyer;
 
     @Column(name = "order_no", nullable = false)
