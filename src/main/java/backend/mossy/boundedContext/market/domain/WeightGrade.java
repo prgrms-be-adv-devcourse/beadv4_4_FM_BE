fa26a5317225ac16ce57718b7160e8e07af29edc
남윤호 <nam3280@naver.com>
2026-01-15 11:43:59 +0900

    feat : 주문/장바구니 도메인 (#19)

---
+++ src/main/java/backend/mossy/boundedContext/market/domain/WeightGrade.java
@@ -0,0 +1,18 @@
+package backend.mossy.boundedContext.market.domain;
+
+import backend.mossy.global.jpa.entity.BaseIdAndTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "MARKET_WEIGHT_GRADE")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class WeightGrade extends BaseIdAndTime {
+    @Column(nullable = false)
+    private String weightGradeName;
+}
