dfdb4bdc2dfe9d231a141cd703e8b9f4a7603b5f
남윤호 <nam3280@naver.com>
2026-01-15 11:43:59 +0900

    feat : 주문/장바구니 도메인 (#19)

---
+++ src/main/java/backend/mossy/boundedContext/market/domain/CartItem.java
@@ -0,0 +1,31 @@
+package backend.mossy.boundedContext.market.domain;
+
+import backend.mossy.global.jpa.entity.BaseIdAndTime;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "MARKET_CART_ITEM")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CartItem extends BaseIdAndTime {
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private int count;
+
+    public CartItem(Cart cart, Long productId, int count) {
+        this.cart = cart;
+        this.productId = productId;
+        this.count = count;
+    }
+}
