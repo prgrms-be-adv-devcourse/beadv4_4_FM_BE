5462c2ca66d1f3260d66fb38c85a8975dcc62421
leehyeonwoo <dlgusdn7706@gmail.com>
2026-01-19 19:26:54 +0900

    Refactor: Repository 네이밍 변경 및 도메인 주체 명시

--- src/main/java/backend/mossy/boundedContext/cash/out/CashUserRepository.java
+++ src/main/java/backend/mossy/boundedContext/cash/out/CashUserRepository.java
@@ -1,9 +1,7 @@
 package backend.mossy.boundedContext.cash.out;
 
-import backend.mossy.boundedContext.cash.domain.wallet.CashUser;
+import backend.mossy.boundedContext.cash.domain.user.CashUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface CashUserRepository extends JpaRepository<CashUser, Long> {
-    Optional<CashUser> findCashUserById(Long id);
 }
---
+++ src/main/java/backend/mossy/boundedContext/cash/out/UserWalletRepository.java
@@ -0,0 +1,12 @@
+package backend.mossy.boundedContext.cash.out;
+
+import backend.mossy.boundedContext.cash.domain.user.UserWallet;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserWalletRepository extends JpaRepository<UserWallet, Long> {
+
+    boolean existsWalletByUserId(Long userId);
+
+    Optional<UserWallet> findWalletByUserId(Long userId);
+}
