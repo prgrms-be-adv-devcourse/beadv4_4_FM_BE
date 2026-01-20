752cdbb6fc53ed966cdd0f665bf075ed2103d608
nam3280 <nam3280@naver.com>
2026-01-20 10:58:33 +0900

    fix : 장바구니 모듈 수정

--- src/main/java/backend/mossy/boundedContext/market/out/cart/CartRepositoryImpl.java
+++ src/main/java/backend/mossy/boundedContext/market/out/cart/CartRepositoryImpl.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-import static backend.mossy.boundedContext.market.domain.QCart.cart;
-import static backend.mossy.boundedContext.market.domain.QCartItem.cartItem;
-import static backend.mossy.boundedContext.market.domain.QProduct.product;
-import static backend.mossy.boundedContext.market.domain.QProductImage.productImage;
+import static backend.mossy.boundedContext.market.domain.cart.QCart.cart;
+import static backend.mossy.boundedContext.market.domain.cart.QCartItem.cartItem;
+import static backend.mossy.boundedContext.market.domain.product.QProduct.product;
+import static backend.mossy.boundedContext.market.domain.product.QProductImage.productImage;
 
 @RequiredArgsConstructor
 public class CartRepositoryImpl implements CartRepositoryCustom {
