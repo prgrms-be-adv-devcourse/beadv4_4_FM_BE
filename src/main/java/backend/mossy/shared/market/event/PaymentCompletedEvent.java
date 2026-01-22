ea3527aa7a95f340666b7cefb7220b2b4506b53e
leehyeonwoo <dlgusdn7706@gmail.com>
2026-01-22 22:12:52 +0900

    Feat: 결제 도메인 이벤트 정의

--- src/main/java/backend/mossy/shared/market/event/PaymentCompletedEvent.java
+++ src/main/java/backend/mossy/shared/market/event/PaymentCompletedEvent.java
@@ -1,6 +1,12 @@
 package backend.mossy.shared.market.event;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public record PaymentCompletedEvent(
-    Long orderId
+    Long orderId,
+    Long buyerId,
+    LocalDateTime paymentDate,
+    BigDecimal amount
 ) {
 }
