43adb73e166dcbbbfa9472fe973e5338e0a28c29
bunohoq <boyoung5187@naver.com>
2026-02-10 00:46:07 +0900

    fix: gateway 에러 수정

--- gateway/src/main/java/com/mossy/GatewayApplication.java
+++ gateway/src/main/java/com/mossy/GatewayApplication.java
@@ -2,11 +2,15 @@ package com.mossy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
-@EnableJpaAuditing
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+        //DB url 문제가 자꾸떠서 임시방편
+})
 @ConfigurationPropertiesScan
 public class GatewayApplication {
     public static void main(String[] args) {
