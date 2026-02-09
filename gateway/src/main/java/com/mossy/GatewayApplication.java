f26ac4386db5f28427702033ad56ffc4a0364e68
bunohoq <boyoung5187@naver.com>
2026-02-10 00:34:09 +0900

    fix: common에서 gateway로 jwt관련 파일 이동 및 WebFlux 기반으로 파일 변경

--- gateway/src/main/java/com/mossy/GatewayApplication.java
+++ gateway/src/main/java/com/mossy/GatewayApplication.java
@@ -2,10 +2,12 @@ package com.mossy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = "com.mossy")
+@SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
