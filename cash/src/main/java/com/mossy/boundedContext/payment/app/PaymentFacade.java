15aa63defcf0fe029e4c5dd36b795fc3bfed51f2
leehyeonwoo <dlgusdn7706@gmail.com>
2026-02-10 11:27:13 +0900

    Refactor : Cash 모듈 이관 및 Market 의존성 분리

--- cash/src/main/java/com/mossy/boundedContext/payment/app/PaymentFacade.java
+++ cash/src/main/java/com/mossy/boundedContext/payment/app/PaymentFacade.java
@@ -19,6 +19,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PaymentFacade {
--- cash/src/main/java/com/mossy/boundedContext/payment/in/dto/request/PaymentConfirmCashRequestDto.java
+++ cash/src/main/java/com/mossy/boundedContext/payment/in/dto/request/PaymentConfirmCashRequestDto.java
@@ -25,5 +25,3 @@ public record PaymentConfirmCashRequestDto(
         }
     }
 }
-
-
