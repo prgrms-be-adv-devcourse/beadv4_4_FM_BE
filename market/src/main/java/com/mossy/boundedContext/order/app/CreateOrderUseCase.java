d6a46e61d2732b0e448b7e575c0ebda13c6a4971
남윤호 <nam3280@naver.com>
2026-02-10 17:46:23 +0900

    [Fix] #132 common 마켓 패키지 수정 (#134)
    * fix : order 생성 API 수정
    
    * chore : 불필요한 코드 삭제
    
    * fix : 주문 생성 API 수정
    
    * feat : 쿠폰 생성 구현
    
    * chore : 불필요한 코드 삭제
    
    * fix : market 패키지 이동 및 common 수정
    
    * fix : market 모듈 컨벤션 수정
    
    * fix : market common 수정
    # Conflicts:
    #	common/src/main/java/com/mossy/shared/market/payload/MarketSellerDto.java
    #	market/src/main/java/com/mossy/boundedContext/order/app/CreateOrderUseCase.java

--- market/src/main/java/com/mossy/boundedContext/order/app/CreateOrderUseCase.java
+++ market/src/main/java/com/mossy/boundedContext/order/app/CreateOrderUseCase.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
