ad69df25890d8313ebed01af64d9c9a401c426c5
nam3280 <nam3280@naver.com>
2026-02-10 15:47:56 +0900

    fix : market 모듈 컨벤션 수정

--- common/src/main/java/com/mossy/shared/market/event/MarketSellerPayload.java
+++
@@ -1,7 +0,0 @@
-package com.mossy.shared.market.event;
-
-import com.mossy.shared.market.payload.MarketSellerDto;
-
-public record MarketSellerPayload(
-        MarketSellerDto seller
-){ }
--- common/src/main/java/com/mossy/shared/market/event/MarketUserPayload.java
+++
@@ -1,7 +0,0 @@
-package com.mossy.shared.market.event;
-
-import com.mossy.shared.market.payload.MarketUserDto;
-
-public record MarketUserPayload(
-        MarketUserDto buyer
-){}
