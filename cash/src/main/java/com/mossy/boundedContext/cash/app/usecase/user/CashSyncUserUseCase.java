4b27145c69a558d22e8359343ef5d66479020aee
leehyeonwoo <dlgusdn7706@gmail.com>
2026-02-10 16:11:34 +0900

    Refactor : 데이터 동기화 로직 리팩토링

--- cash/src/main/java/com/mossy/boundedContext/cash/app/usecase/user/CashSyncUserUseCase.java
+++ cash/src/main/java/com/mossy/boundedContext/cash/app/usecase/user/CashSyncUserUseCase.java
@@ -2,7 +2,7 @@ package com.mossy.boundedContext.cash.app.usecase.user;
 
 import com.mossy.boundedContext.cash.app.mapper.CashMapper;
 import com.mossy.boundedContext.cash.domain.user.CashUser;
-import com.mossy.boundedContext.cash.in.dto.common.CashUserDto;
+import com.mossy.boundedContext.cash.in.dto.command.CashUserDto;
 import com.mossy.boundedContext.cash.out.user.CashUserRepository;
 import com.mossy.global.eventPublisher.EventPublisher;
 import com.mossy.boundedContext.cash.in.dto.event.CashUserCreatedEvent;
@@ -22,20 +22,11 @@ public class CashSyncUserUseCase {
     public void syncUser(CashUserDto user) {
         cashUserRepository.findById(user.id())
             .ifPresentOrElse(
-                existingUser -> existingUser.update(
-                    user.name(),
-                    user.email(),
-                    user.address(),
-                    user.nickname(),
-                    user.profileImage(),
-                    user.status(),
-                    user.latitude(),
-                    user.longitude()
-                ),
+                existingUser -> existingUser.update(user),
                 () -> {
                     CashUser newUser = cashUserRepository.save(mapper.toEntity(user));
                     eventPublisher.publish(
-                        new CashUserCreatedEvent(mapper.toPayload(newUser))
+                        new CashUserCreatedEvent(mapper.toDto(newUser))
                     );
                 }
             );
