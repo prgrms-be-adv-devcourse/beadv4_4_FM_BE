0177804737e3fdd9168bb687af4bada885e55d63
bunohoq <boyoung5187@naver.com>
2026-02-11 14:25:29 +0900

    refactor: 컨벤션으로 수정

--- member/src/main/java/com/mossy/boundedContext/in/dto/UserInfoDto.java
+++ member/src/main/java/com/mossy/boundedContext/in/dto/UserInfoDto.java
@@ -1,17 +1,15 @@
 package com.mossy.boundedContext.in.dto;
 
 import com.mossy.shared.member.domain.enums.SellerRequestStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class UserInfoDto {
-    private Long userId;
-    private String nickname;
-    private String username;
-    private SellerRequestStatus status;
+public record UserInfoDto(
+        Long userId,
+        String nickname,
+        String username,
+        SellerRequestStatus status
+)
 
+{
     public static UserInfoDto of(Long userId, String nickname, String username,SellerRequestStatus status) {
         return new UserInfoDto(userId, nickname, username, status);
     }
