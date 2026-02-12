8199a1751d0aefcaebcd637982166edafa69fe41
leehyeonwoo <dlgusdn7706@gmail.com>
2026-02-12 14:15:03 +0900

    Feat: RecommendItem 엔티티 및 ReactiveCrudRepository 인터페이스 추가

--- ai/src/main/java/com/mossy/boundedContext/recommendation/domain/RecommendItem.java
+++ ai/src/main/java/com/mossy/boundedContext/recommendation/domain/RecommendItem.java
@@ -1,4 +1,24 @@
 package com.mossy.boundedContext.recommendation.domain;
 
-public class RecommendItem {
+import com.mossy.global.jpa.entity.BaseManualIdAndTime;
+import lombok.*;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("recommend_item")
+@Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RecommendItem extends BaseManualIdAndTime {
+
+    @Column("product_id")
+    private Long productId;
+
+    // 검색 대상 텍스트 (상품명 + 설명)
+    private String content;
+
+    // pgvector 데이터
+    @Column("vector_data")
+    private String vectorData;
 }
--- ai/src/main/java/com/mossy/boundedContext/recommendation/out/RecommendItemRepository.java
+++ ai/src/main/java/com/mossy/boundedContext/recommendation/out/RecommendItemRepository.java
@@ -2,6 +2,13 @@ package com.mossy.boundedContext.recommendation.out;
 
 import com.mossy.boundedContext.recommendation.domain.RecommendItem;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface RecommendItemRepository extends ReactiveCrudRepository<RecommendItem, Long> {
+
+    // Todo 백터 유사도 검색 쿼리문 작성
+    Flux<RecommendItem> findTopSimilarItems(String requestVector, int limit);
+
+    Mono<Boolean> existsByProductId(Long productId);
 }
