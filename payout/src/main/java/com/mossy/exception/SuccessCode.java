8906058b151ca4915ef5db7387545a05f6217ce0
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-16 23:45:55 +0900

    Feat : 에러코드 성공코드 빌드 그래들 수정 및 추가

--- payout/src/main/java/com/mossy/exception/SuccessCode.java
+++ payout/src/main/java/com/mossy/exception/SuccessCode.java
@@ -1,7 +1,15 @@
 package com.mossy.exception;
 
+import com.mossy.global.exception.BaseCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-public enum SuccessCode {
+@RequiredArgsConstructor
+public enum SuccessCode implements BaseCode {
+    PAYOUT_DAILY_WALLET_NOTHING_TO_PROCESS(200, "처리할 정산이 없습니다."),
+    PAYOUT_DAILY_WALLET_CREDIT_PROCESSED(201, "판매자 지급 처리가 완료되었습니다.");
+
+    private final int status;
+    private final String msg;
 }
