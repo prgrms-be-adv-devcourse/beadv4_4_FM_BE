06fc3d2b47268ba4ac333602dbd59fd795323b42
boyoung <115682037+bunohoq@users.noreply.github.com>
2026-02-16 11:39:54 +0900

    [Feat]: #109 인증 서비스 고도화(RTR) 및 만료 토큰 필터링 이슈 검토 요청[Merge 금지] (#160)
    * refactor: AuthFacade 책임 분리(재발급/로그아웃 useCase 분리)
    
    * refactor: 마이페이지 xuserid 헤더로 변경 및 마이페이지 내용 수정
    
    * feat: Refresh Token 해싱을 위한 pepper 외부화
    
    * feat: Auth 모듈의 FeignClient 통신을 위한 Member 내부 API 추가
    
    * fix: 토큰 재발급(Reissue) 로직 오류 수정 및 안정화
    
    * debug: 필터링 로직 오류 수정 시도 및 피드백 요청

--- member/src/main/java/com/mossy/boundedContext/in/MemberInternalController.java
+++ member/src/main/java/com/mossy/boundedContext/in/MemberInternalController.java
@@ -2,14 +2,12 @@ package com.mossy.boundedContext.in;
 
 import com.mossy.boundedContext.app.user.UserFacade;
 import com.mossy.boundedContext.out.external.dto.request.MemberVerifyRequest;
+import com.mossy.boundedContext.out.external.dto.response.MemberAuthInfoResponse;
 import com.mossy.boundedContext.out.external.dto.response.MemberVerifyExternResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Internal-Member", description = "시스템 내부 서비스 간 회원 통신 API")
 @RestController
@@ -23,5 +21,11 @@ public class MemberInternalController {
     @PostMapping("/verify")
     public MemberVerifyExternResponse verify(@RequestBody MemberVerifyRequest req) {
         return userFacade.verifyMember(req.email(), req.password());
+    }
+
+    @Operation(summary = "회원 인증 정보 조회 (내부전용)")
+    @GetMapping("/{userId}")
+    public MemberAuthInfoResponse getAuthInfo(@PathVariable Long userId) {
+        return userFacade.getAuthInfo(userId);
     }
 }
