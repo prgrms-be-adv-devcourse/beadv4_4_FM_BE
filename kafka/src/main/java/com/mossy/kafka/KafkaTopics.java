202b09e57478014446ffeccbb855621765ff9c24
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-17 17:54:46 +0900

    Feat : 주문에서 받을 카프카 토픽 생성

--- kafka/src/main/java/com/mossy/kafka/KafkaTopics.java
+++ kafka/src/main/java/com/mossy/kafka/KafkaTopics.java
@@ -4,4 +4,6 @@ public class KafkaTopics {
     public static final String PAYMENT_REFUND = "payment.refund";
     public static final String ORDER_CANCEL = "order.cancel";
     public static final String PAYOUT_WALLET_CREDIT = "payout.wallet.credit";
+    public static final String ORDER_PAID = "order.paid";
+    public static final String ORDER_REFUNDED = "order.refunded";
 }
