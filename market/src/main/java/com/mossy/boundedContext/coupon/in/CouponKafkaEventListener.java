bd12ff917448d18f107090ec9c055fa43b3c0f94
nam3280 <nam3280@naver.com>
2026-02-19 09:07:36 +0900

    chore : 불필요한 코드 삭제

--- market/src/main/java/com/mossy/boundedContext/coupon/in/CouponKafkaEventListener.java
+++ market/src/main/java/com/mossy/boundedContext/coupon/in/CouponKafkaEventListener.java
@@ -4,7 +4,6 @@ import com.mossy.boundedContext.coupon.app.CouponFacade;
 import com.mossy.kafka.KafkaTopics;
 import com.mossy.shared.market.event.CouponUseRequestedEvent;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
