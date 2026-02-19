59ba7694d676e90f339ad89244371eedad026024
nam3280 <nam3280@naver.com>
2026-02-19 09:07:19 +0900

    feat : 카프카 yml 추가

--- market/src/main/java/com/mossy/boundedContext/coupon/in/CouponKafkaEventListener.java
+++ market/src/main/java/com/mossy/boundedContext/coupon/in/CouponKafkaEventListener.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CouponKafkaEventListener {
