b8af28aa813d17407a9482a148483ad9d519c95e
leehyeonwoo <dlgusdn7706@gmail.com>
2026-02-20 10:53:58 +0900

    Refactor: Repository 반환 타입 List → Page로 변경

--- cash/src/main/java/com/mossy/boundedContext/cash/out/seller/SellerCashLogRepository.java
+++ cash/src/main/java/com/mossy/boundedContext/cash/out/seller/SellerCashLogRepository.java
@@ -1,10 +1,10 @@
 package com.mossy.boundedContext.cash.out.seller;
 
 import com.mossy.boundedContext.cash.domain.seller.SellerCashLog;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SellerCashLogRepository extends JpaRepository<SellerCashLog, Long> {
-    List<SellerCashLog> findAllBySellerIdOrderByCreatedAtDesc(Long sellerId);
-
+    Page<SellerCashLog> findAllBySellerIdOrderByCreatedAtDesc(Long sellerId, Pageable pageable);
 }
