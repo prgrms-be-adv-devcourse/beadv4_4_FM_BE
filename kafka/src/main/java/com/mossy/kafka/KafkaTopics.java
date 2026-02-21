61970d08d4d0dc414c7ec9d1a623a0542541af16
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-21 21:54:20 +0900

    Feat : 카프카 추가

--- kafka/src/main/java/com/mossy/kafka/KafkaTopics.java
+++ kafka/src/main/java/com/mossy/kafka/KafkaTopics.java
@@ -10,4 +10,5 @@ public class KafkaTopics {
     public static final String SELLER_JOINED = "seller.joined";
     public static final String SELLER_UPDATED = "seller.updated";
     public static final String ORDER_PURCHASE_CONFIRMED = "order.purchase.confirmed";
+    public static final String PAYOUT_WALLET_CREDIT = "payout.wallet.credit";
 }
