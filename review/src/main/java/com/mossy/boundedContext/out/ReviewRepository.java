7a3a61e61cc63f4084cf0991914bd525f2e67090
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-23 02:29:33 +0900

    Feat: 리뷰아이템 레포 생성

--- review/src/main/java/com/mossy/boundedContext/out/ReviewRepository.java
+++ review/src/main/java/com/mossy/boundedContext/out/ReviewRepository.java
@@ -4,4 +4,5 @@ import com.mossy.boundedContext.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    boolean existsByOrderItemId(Long orderItemId);
 }
