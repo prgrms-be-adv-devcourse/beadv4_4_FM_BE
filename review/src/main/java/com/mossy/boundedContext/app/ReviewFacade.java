be84434d40e0bbc39d1ad88ac55b1bca062480db
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-23 02:32:53 +0900

    Feat : 리뷰 파사타 추가

--- review/src/main/java/com/mossy/boundedContext/app/ReviewFacade.java
+++ review/src/main/java/com/mossy/boundedContext/app/ReviewFacade.java
@@ -1,9 +1,23 @@
 package com.mossy.boundedContext.app;
 
+import com.mossy.boundedContext.in.dto.request.WriteReviewRequest;
+import com.mossy.boundedContext.in.dto.response.ReviewResponse;
+import com.mossy.shared.market.event.OrderPurchaseConfirmedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewFacade {
+
+    private final CreateReviewableItemUseCase createReviewableItemUseCase;
+    private final WriteReviewUseCase writeReviewUseCase;
+
+    public void handleOrderPurchaseConfirmed(OrderPurchaseConfirmedEvent event) {
+        createReviewableItemUseCase.create(event);
+    }
+
+    public ReviewResponse writeReview(Long orderItemId, WriteReviewRequest request) {
+        return writeReviewUseCase.write(orderItemId, request);
+    }
 }
