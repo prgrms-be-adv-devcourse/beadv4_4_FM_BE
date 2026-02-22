23a662626d420f6f4bfc42eb837235b89de14503
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-23 02:29:45 +0900

    Feat : 성공 메시지 생성

--- review/src/main/java/com/mossy/exception/SuccessCode.java
+++ review/src/main/java/com/mossy/exception/SuccessCode.java
@@ -4,4 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum SuccessCode {
+    REVIEW_CREATED(201, "리뷰가 작성되었습니다.");
+
+    private final int status;
+    private final String msg;
+
+    SuccessCode(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public int getStatus() { return status; }
+    public String getMsg() { return msg; }
 }
