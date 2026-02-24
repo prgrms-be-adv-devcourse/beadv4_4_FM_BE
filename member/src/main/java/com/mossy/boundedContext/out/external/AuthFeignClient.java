401396e588fbff48d6d9a1306d70497e77681b07
bunohoq <boyoung5187@naver.com>
2026-02-24 18:04:26 +0900

    fix: 도커로 feignclient 연결 안되는거 해결

--- member/src/main/java/com/mossy/boundedContext/out/external/AuthFeignClient.java
+++ member/src/main/java/com/mossy/boundedContext/out/external/AuthFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "auth", url = "http://localhost:8086")
+@FeignClient(name = "auth", url = "${mossy.feign.url}")
 public interface AuthFeignClient {
 
 
