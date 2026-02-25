14d4edc72e8527b1115dc3505042516abd2b836e
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-26 00:49:50 +0900

    Feat : 구매자 페이징 쿼리 추가

--- member/src/main/java/com/mossy/boundedContext/out/repository/user/UserRepository.java
+++ member/src/main/java/com/mossy/boundedContext/out/repository/user/UserRepository.java
@@ -1,10 +1,13 @@
 package com.mossy.boundedContext.out.repository.user;
 
 import com.mossy.boundedContext.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -38,4 +41,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
+
+    @Query("""
+        SELECT u FROM User u
+        WHERE NOT EXISTS (
+            SELECT ur FROM UserRole ur
+            WHERE ur.user = u AND ur.role.code = com.mossy.shared.member.domain.role.RoleCode.SELLER
+        ) AND u.status != com.mossy.shared.member.domain.enums.UserStatus.DELETED
+    """)
+    Page<User> findBuyers(Pageable pageable);
+
+    List<User> findAllByIdIn(List<Long> ids);
 }
