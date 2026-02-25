fe5b9a7fac7381d567bc9b9789b1c8529576b97c
YoungE <56716209+kchy0625@users.noreply.github.com>
2026-02-25 22:12:37 +0900

    Feat : 리뷰 작성하지 않은 목록 조회 레포 추가

--- review/src/main/java/com/mossy/boundedContext/out/ReviewableItemRepository.java
+++ review/src/main/java/com/mossy/boundedContext/out/ReviewableItemRepository.java
@@ -3,9 +3,11 @@ package com.mossy.boundedContext.out;
 import com.mossy.boundedContext.domain.ReviewableItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewableItemRepository extends JpaRepository<ReviewableItem, Long> {
     boolean existsByOrderItemId(Long orderItemId);
     Optional<ReviewableItem> findByOrderItemId(Long orderItemId);
+    List<ReviewableItem> findByBuyerIdAndReviewedFalseOrderByCreatedAtDesc(Long buyerId);
 }
