e38dacd2fd6000c5fbde2856b865a2967e092130
nam3280 <nam3280@naver.com>
2026-02-26 17:47:12 +0900

    feat : 결제 취소 이벤트 구독

--- market/src/main/java/com/mossy/boundedContext/order/in/OrderKafkaEventListener.java
+++ market/src/main/java/com/mossy/boundedContext/order/in/OrderKafkaEventListener.java
@@ -3,6 +3,7 @@ package com.mossy.boundedContext.order.in;
 import com.mossy.boundedContext.order.app.OrderFacade;
 import com.mossy.kafka.KafkaTopics;
 import com.mossy.shared.cash.event.PaymentCompletedEvent;
+import com.mossy.shared.cash.event.PaymentTossRefundEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -18,5 +19,10 @@ public class OrderKafkaEventListener {
     @KafkaListener(topics = KafkaTopics.PAYMENT_COMPLETED)
     public void handlePaymentCompletedEvent(PaymentCompletedEvent event) {
         orderFacade.completePayment(event);
+    }
+
+    @KafkaListener(topics = KafkaTopics.ORDER_CANCEL)
+    public void handleOrderCanceledEvent(PaymentTossRefundEvent event) {
+        orderFacade.cancelOrder(event.response());
     }
 }
