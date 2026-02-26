e3107e5cd14c3104f3f0594723782c8c47893ddc
ddubuk228 <practice2280@gmail.com>
2026-02-26 17:07:50 +0900

    [Fix] CORS 필터 수정

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation(project(":common")) {
         exclude(group = "org.springframework.boot", module = "spring-boot-starter-web")
         exclude(group = "org.springframework.boot", module = "spring-boot-starter-data-jpa")
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-data-redis")
     }
 
     // API Gateway
